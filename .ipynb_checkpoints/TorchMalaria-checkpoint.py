1b47b61b4ee7ffd5b6a18dcc697c42dd88b2077d
Simcha <simchavanhelvoort@gmail.com>
2019-07-03 12:05:17 +0200

    Model done

--- .ipynb_checkpoints/TorchMalaria-checkpoint.py
+++ .ipynb_checkpoints/TorchMalaria-checkpoint.py
@@ -7,14 +7,26 @@ import torch.nn.functional as F
 from torch.utils.data import DataLoader, Dataset
 import matplotlib.pyplot as plt
 from skimage.transform import resize
+from sklearn.model_selection import train_test_split
+
 
 def GetNumCorrect(pred, labels):
     return pred.argmax(dim = 1).eq(labels).sum()
 
 class DataMalaria(Dataset):
-    def __init__(self, filepath, transform = None):
+    """
+    filepath = directory naar afbeeldingen
+    transform = eventuele transformatie naar pytorch tensors
+    TTS = train test split. Het splitsen tussen trainingset en testset
+    """
+    def __init__(self, filepath, transform = None, TTS = True):
         self.data = pd.read_csv(filepath, sep = ";")
         self.transform = transform
+        self.train = self.data
+        self.test = self.data
+        if TTS:
+            self.trainTestSplit()
+            self.trainmode()
     
     def __len__(self):
         return len(self.data)
@@ -37,3 +49,13 @@ class DataMalaria(Dataset):
         plt.imshow(torch.transpose(image, -3, 2))
         plt.title("label: {}".format(label))
         plt.show()
+        
+    def trainTestSplit(self, p = 0.25):
+        self.train, self.test = train_test_split(self.data, test_size = p)
+        
+    def testmode(self):
+        self.data = self.test
+        
+    def trainmode(self):
+        self.data = self.train
+    
